883f3e488dfd6a22ecd272dde2f1f299959307e1
natserract <benjaminstwo@gmail.com>
2022-06-17 14:48:43 +0800

    feat: add `RecordAccessor` types, and func

--- src/Tapi/Utils.hs
+++ src/Tapi/Utils.hs
@@ -4,11 +4,15 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE EmptyDataDeriving #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# OPTIONS_GHC -Wno-missing-methods #-}
+{-# LANGUAGE KindSignatures #-}
 
 module Tapi.Utils 
   ( Generic
   , All
   , (:=)
+  , RecordAccessor (..)
+  , getRecord
   ) where
 
 import Data.Kind (Constraint)
@@ -21,12 +25,17 @@ data All = All
 
 -- * -> *
 infixl 4 :=
-data (:=) sh deriving (Eq, Show)
-instance Num ((:=) m) where
-  (+) = (+)
-  (*) = (*)
-  abs = abs
-  signum = signum
-  fromInteger = fromInteger
-  negate = negate
-  
+data (:=) sh deriving (Eq, Show, Num)
+
+-- | Acceess record field
+type RecordAccessor (r :: *) a = r -> a -> a
+
+-- | Get the value of a field.
+{--
+  recordS = RecordName { fieldName = value, ...}
+  getRecord record field
+
+  type :: ^ RecordAccessor RecordT (RecordT -> FieldT)
+-}
+getRecord :: r -> fie -> RecordAccessor r fie
+getRecord = getRecord
